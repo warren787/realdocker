e23d93b923318aabc2cc4777787c15c38417b7a5
warren787 <jessicawarren787@outlook.com>
2024-09-05 07:45:59 -0700

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,9 +2,6 @@
 
 # Define Environment Variables
 export UUID=$(openssl rand -hex 16 | awk '{print substr($0,1,8)"-"substr($0,9,4)"-"substr($0,13,4)"-"substr($0,17,4)"-"substr($0,21,12)}')
-export NEZHA_SERVER=${NEZHA_SERVER:-'nz.f4i.cn'} 
-export NEZHA_PORT=${NEZHA_PORT:-'5555'}     
-export NEZHA_KEY=${NEZHA_KEY:-''} 
 export FILE_PATH=${FILE_PATH:-'./app'}
 export SNI=${SNI:-'www.yahoo.com'}
 
@@ -98,23 +95,6 @@ generate_config
 
 # running files
 run() {
-  if [ -e "${FILE_PATH}/npm" ]; then
-    chmod 777 "${FILE_PATH}/npm"
-    tlsPorts=("443" "8443" "2096" "2087" "2083" "2053")
-    if [[ "${tlsPorts[*]}" =~ "${NEZHA_PORT}" ]]; then
-      NEZHA_TLS="--tls"
-    else
-      NEZHA_TLS=""
-    fi
-    if [ -n "$NEZHA_SERVER" ] && [ -n "$NEZHA_PORT" ] && [ -n "$NEZHA_KEY" ]; then
-        nohup ${FILE_PATH}/npm -s ${NEZHA_SERVER}:${NEZHA_PORT} -p ${NEZHA_KEY} ${NEZHA_TLS} >/dev/null 2>&1 &
-		sleep 2
-        pgrep -x "npm" > /dev/null && echo -e "\e[1;32mnpm is running\e[0m" || { echo -e "\e[1;35mnpm is not running, restarting...\e[0m"; pkill -x "npm" && nohup "${FILE_PATH}/npm" -s ${NEZHA_SERVER}:${NEZHA_PORT} -p ${NEZHA_KEY} ${NEZHA_TLS} >/dev/null 2>&1 & sleep 2; echo -e "\e[1;32mnpm restarted\e[0m"; }
-    else
-        echo -e "\e[1;35mNEZHA variable is empty,skiping runing\e[0m"
-    fi
-  fi
-
   if [ -e "${FILE_PATH}/web" ]; then
     chmod 777 "${FILE_PATH}/web"
     nohup ${FILE_PATH}/web -c ${FILE_PATH}/config.json >/dev/null 2>&1 &
